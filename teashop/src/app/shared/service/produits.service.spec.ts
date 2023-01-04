4a9140bf48ed46790d026dc699954838c0d0d874
Daniel Pinson <daniel.pinson19@gmail.com>
2023-01-04 23:12:14 +0100

    début tests unitaires pour produits service

--- teashop/src/app/shared/service/produits.service.spec.ts
+++ teashop/src/app/shared/service/produits.service.spec.ts
@@ -3,24 +3,56 @@ import { TestBed } from '@angular/core/testing';
 import { AngularFireModule } from '@angular/fire/compat';
 import { environment } from 'src/environments/environment';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { of } from 'rxjs';
 
 import { ProduitsService } from './produits.service';
 
 describe('ProduitsService', () => {
-  let service: ProduitsService;
+  describe('Test unitaire', () => {
+      let service: ProduitsService;
+      let angularFirestoreSpy: any;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      imports : [AngularFireModule.initializeApp(environment.firebase)],
-      providers: [
-        {provide: AngularFirestore, useClass: AngularFirestoreStub}
-      ]
-    });
-    service = TestBed.inject(ProduitsService);
+
+      beforeEach(() => {
+          const spyDB = jasmine.createSpyObj('AngularFirestore', ['collection']);
+          TestBed.configureTestingModule({
+              providers: [
+                  ProduitsService,
+                  {provide: AngularFirestore, useValue: spyDB}
+              ]
+          });
+          service = TestBed.inject(ProduitsService);
+          angularFirestoreSpy = TestBed.inject(AngularFirestore) as jasmine.SpyObj<AngularFirestore>;
+      });
+
+      it('Créée ProduitsService en indépendance', () => {
+          expect(service).toBeTruthy();
+      });
+
+      it('Récupère la base de donnée', () => {
+        expect(service.getDatabase()).toBe(angularFirestoreSpy);
+      });
+
+      it('Récupère tous les produits', () => {
+        angularFirestoreSpy.collection.and.returnValue({ snapshotChanges: () => of([]) });
+        service.getAllProduits();
+        expect(angularFirestoreSpy.collection).toHaveBeenCalledWith('/Produits');
+      });
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  describe('Test d\'intégration', () => {
+      let service: ProduitsService;
+
+      beforeEach(() => {
+          TestBed.configureTestingModule({
+              imports : [AngularFireModule.initializeApp(environment.firebase)],
+          });
+          service = TestBed.inject(ProduitsService);
+      });
+
+      it('Création ProduitsService avec dépendance', () => {
+          expect(service).toBeTruthy();
+      });
   });
 });
 class AngularFirestoreStub{}

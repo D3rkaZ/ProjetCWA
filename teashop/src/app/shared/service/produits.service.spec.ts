fad7b6f240614fb3c40c90c5ff665850a761b50c
Daniel Pinson <daniel.pinson19@gmail.com>
2023-01-05 23:14:21 +0100

    pas d'avancement

--- teashop/src/app/shared/service/produits.service.spec.ts
+++ teashop/src/app/shared/service/produits.service.spec.ts
@@ -1,58 +1,68 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, HttpTestingController } from '@angular/core/testing';
 
 import { AngularFireModule } from '@angular/fire/compat';
 import { environment } from 'src/environments/environment';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
-import { of } from 'rxjs';
+import { map, of } from 'rxjs';
 
 import { ProduitsService } from './produits.service';
+import { Produit } from '../modele/produit';
 
 describe('ProduitsService', () => {
-  describe('Test unitaire', () => {
-      let service: ProduitsService;
-      let angularFirestoreSpy: any;
+    describe('Test unitaire', () => {
+        let service: ProduitsService;
+        let angularFirestoreSpy: any;
 
 
-      beforeEach(() => {
-          const spyDB = jasmine.createSpyObj('AngularFirestore', ['collection']);
-          TestBed.configureTestingModule({
-              providers: [
-                  ProduitsService,
-                  {provide: AngularFirestore, useValue: spyDB}
-              ]
-          });
-          service = TestBed.inject(ProduitsService);
-          angularFirestoreSpy = TestBed.inject(AngularFirestore) as jasmine.SpyObj<AngularFirestore>;
-      });
+        beforeEach(() => {
+            const spyDB = jasmine.createSpyObj('AngularFirestore', ['collection']);
+            TestBed.configureTestingModule({
+                providers: [
+                    ProduitsService,
+                    {provide: AngularFirestore, useValue: spyDB}
+                ]
+            });
+            service = TestBed.inject(ProduitsService);
+            angularFirestoreSpy = TestBed.inject(AngularFirestore) as jasmine.SpyObj<AngularFirestore>;
+        });
 
-      it('Créée ProduitsService en indépendance', () => {
-          expect(service).toBeTruthy();
-      });
+        it('Créée ProduitsService en indépendance', () => {
+            expect(service).toBeTruthy();
+        });
 
-      it('Récupère la base de donnée', () => {
-        expect(service.getDatabase()).toBe(angularFirestoreSpy);
-      });
+        it('Récupère la base de donnée', () => {
+            expect(service.getDatabase()).toBe(angularFirestoreSpy);
+        });
 
-      it('Récupère tous les produits', () => {
-        angularFirestoreSpy.collection.and.returnValue({ snapshotChanges: () => of([]) });
-        service.getAllProduits();
-        expect(angularFirestoreSpy.collection).toHaveBeenCalledWith('/Produits');
-      });
-  });
+        it('Demande à la base de donnée de donner les produits', () => {
+            angularFirestoreSpy.collection.and.returnValue({ snapshotChanges: () => of([]) });
+            service.getAllProduits();
+            expect(angularFirestoreSpy.collection).toHaveBeenCalledWith('/Produits');
+        });
+    });
 
-  describe('Test d\'intégration', () => {
-      let service: ProduitsService;
+    describe('Test d\'intégration', () => {
+        let service: ProduitsService;
+        let httpMock = HttpTestingController;
 
-      beforeEach(() => {
-          TestBed.configureTestingModule({
-              imports : [AngularFireModule.initializeApp(environment.firebase)],
-          });
-          service = TestBed.inject(ProduitsService);
-      });
+        beforeEach(() => {
+            TestBed.configureTestingModule({
+                imports : [AngularFireModule.initializeApp(environment.firebase),
+                    HttpTestingController],
+            });
+            service = TestBed.inject(ProduitsService);
+            httpMock = TestBed.inject(HttpTestingController);
+        });
 
-      it('Création ProduitsService avec dépendance', () => {
-          expect(service).toBeTruthy();
-      });
-  });
+        it('Création ProduitsService avec dépendance', () => {
+            expect(service).toBeTruthy();
+        });
+
+        //it('Récupère tous les produits', () => {
+        //    const mockResponse = [ {
+        //        id: "1", nom: "Produit1", titre: "thé fuité", url:"lambda", type: "tp", parfum: "fuité",
+        //        pays: "france", prix:6, description:"description", suggestion:"sugg", qte: 0, qteStock:12
+        //    }];
+        //});
+    });
 });
-class AngularFirestoreStub{}
